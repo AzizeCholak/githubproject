ab939f4d051b957b79db60e8dd9e335597a699c0
Azize Cholak <azizecholak1303@gmail.com>
2022-02-11 19:39:40 +0000

    My new class

--- src/Fall2021/Second_Class.java
+++ src/Fall2021/Second_Class.java
@@ -4,5 +4,7 @@ public class Second_Class {
     public static void main(String[] args){
         System.out.println("new page second class");
         System.out.println("how are you");
+        System.out.println("fine");
+        System.out.println("failure");
     }
 }
