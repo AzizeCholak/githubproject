64036c0539232850ace4746a07039ee051db2d9c
Azize Cholak <azizecholak1303@gmail.com>
2022-02-11 19:57:24 +0000

    My new class

--- src/Fall2021/Main.java
+++ src/Fall2021/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[]args){
         System.out.println("1st class");
-        System.out.println("2nd step");
+        System.out.println(" step");
         System.out.println("third step");
-        System.out.println("forth step");
+        System.out.println(" step");
     }
 }
