7c677ad46d41410e0a8a755a83edb4741079a7ac
Azize Cholak <95502532+AzizeCholak@users.noreply.github.com>
2022-02-11 19:56:59 +0000

    Update Main.java

--- src/Fall2021/Main.java
+++ src/Fall2021/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("2nd step");
         System.out.println("third step");
         System.out.println("forth step");
+         System.out.println("");
     }
 }
